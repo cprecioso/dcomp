66dfa7e0daf0f2aee37eaa009da57fb40e3dedda
Carlos Precioso <511681+cprecioso@users.noreply.github.com>
2022-04-01 16:52:29 +0200

    Do not explicitly add names to volumes

--- packages/defs/src/entities/volume.ts
+++ packages/defs/src/entities/volume.ts
@@ -2,16 +2,12 @@ import { DefinitionsVolume as VolumeSpec } from "../spec";
 import { OverrideProperties } from "../util";
 import { BaseEntity } from "./base";
 
-export interface VolumeDefinition
-  extends OverrideProperties<VolumeSpec, { name: never }> {}
+export interface VolumeDefinition extends OverrideProperties<VolumeSpec, {}> {}
 
 export class Volume extends BaseEntity<"volume", VolumeDefinition> {
   type = "volume" as const;
 
   get spec(): VolumeSpec {
-    return {
-      ...this._definition,
-      name: this.name,
-    };
+    return this._definition;
   }
 }
